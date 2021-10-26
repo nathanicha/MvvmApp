2c3589e5dd069eca189a13aba51999b802f7e030
Nathanicha Lwd <35954605+nathanicha@users.noreply.github.com>
2021-10-26 10:26:54 +0700

    feat: initial main application

--- app/src/main/java/com/natlwd/mvvmapp/MainApplication.kt
+++ app/src/main/java/com/natlwd/mvvmapp/MainApplication.kt
@@ -1,4 +1,18 @@
 package com.natlwd.mvvmapp
 
-class MainApplication {
+import android.app.Application
+import android.content.Context
+
+class MainApplication : Application() {
+    companion object {
+        lateinit var mainApplication: MainApplication
+        fun getContext(): Context {
+            return mainApplication.applicationContext
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        mainApplication = this
+    }
 }
